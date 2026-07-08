5b19dedb540c63d0ef050c0894e9b413d6d25021
Harsh Doshi <harshdoshi207@gmail.com>
2026-07-08 13:03:16 +0530

    updated

---
+++ UserControlDemo/UserControls/EmployeeCard.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UserControlDemo.UserControls
+{
+    public partial class EmployeeCard : UserControl
+    {
+        public EmployeeCard()
+        {
+            InitializeComponent();
+        }
+
+        // EmployeeName
+
+        public static readonly DependencyProperty EmployeeNameProperty =
+            DependencyProperty.Register(
+                nameof(EmployeeName),
+                typeof(string),
+                typeof(EmployeeCard),
+                new PropertyMetadata(""));
+
+        public string EmployeeName
+        {
+            get => (string)GetValue(EmployeeNameProperty);
+            set => SetValue(EmployeeNameProperty, value);
+        }
+
+        // Department
+
+        public static readonly DependencyProperty DepartmentProperty =
+            DependencyProperty.Register(
+                nameof(Department),
+                typeof(string),
+                typeof(EmployeeCard),
+                new PropertyMetadata(""));
+
+        public string Department
+        {
+            get => (string)GetValue(DepartmentProperty);
+            set => SetValue(DepartmentProperty, value);
+        }
+
+        // Salary
+
+        public static readonly DependencyProperty SalaryProperty =
+            DependencyProperty.Register(
+                nameof(Salary),
+                typeof(string),
+                typeof(EmployeeCard),
+                new PropertyMetadata(""));
+
+        public string Salary
+        {
+            get => (string)GetValue(SalaryProperty);
+            set => SetValue(SalaryProperty, value);
+        }
+    }
+}
---
+++ UserControlDemo/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using UserControlDemo.Models;
+
+namespace UserControlDemo.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public ObservableCollection<Employee> Employees
+        {
+            get;
+            set;
+        }
+
+        public EmployeeViewModel()
+        {
+            Employees = new ObservableCollection<Employee>();
+
+            Employees.Add(new Employee
+            {
+                Name = "Harsh",
+                Department = "IT",
+                Salary = "₹50,000"
+            });
+
+            Employees.Add(new Employee
+            {
+                Name = "Rahul",
+                Department = "HR",
+                Salary = "₹40,000"
+            });
+
+            Employees.Add(new Employee
+            {
+                Name = "Amit",
+                Department = "Finance",
+                Salary = "₹60,000"
+            });
+
+            Employees.Add(new Employee
+            {
+                Name = "krish",
+                Department = "Seo",
+                Salary = "₹30,000"
+            });
+        }
+    }
+}
