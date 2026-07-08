e6f71294552aa443e41555a746dc46353537eb4f
Harsh Doshi <harshdoshi207@gmail.com>
2026-07-08 13:15:17 +0530

    model views complete

---
+++ POC13_NavigationApp/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using POC13_NavigationApp.Models;
+
+namespace POC13_NavigationApp.ViewModels
+{
+    public class ProductsViewModel
+    {
+        public ObservableCollection<Product> Products
+        {
+            get;
+            
+        }
+
+        public ProductsViewModel()
+        {
+            Products = new ObservableCollection<Product>()
+            {
+                    new Product()
+                    {
+                        Name="Laptop",
+                        Price=50000
+                    },
+
+                    new Product()
+                    {
+                        Name="Keyboard",
+                        Price=1500
+                    },
+
+                    new Product()
+                    {
+                        Name="Mouse",
+                        Price=700
+                    },
+
+                    new Product()
+                    {
+                        Name="Monitor",
+                        Price=12000
+                    }
+            };
+        }
+    }
+}
